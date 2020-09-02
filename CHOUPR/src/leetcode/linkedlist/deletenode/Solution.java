7e3f44d2f71bae56f4f4486debf69e1694ffee54
readgin79 <fire1017@msn.com>
2020-09-02 17:21:56 +0800

    deleteNode 解答

--- CHOUPR/src/leetcode/linkedlist/deletenode/Solution.java
+++ CHOUPR/src/leetcode/linkedlist/deletenode/Solution.java
@@ -18,9 +18,9 @@ public class Solution {
 		listnode.next.next.next = new ListNode(9);
 		
 		Solution solution = new Solution(listnode);
-		solution.deleteNode(new ListNode(1));
+		solution.deleteNode(listnode.next.next);
 		
-		System.out.println(solution.tempNode.next);
+		System.out.println("===== [4,5,1,9]=======");
 		while(solution.tempNode!=null) {
 			System.out.println("....>"+solution.tempNode.val);
 			solution.tempNode = solution.tempNode.next;
@@ -28,30 +28,9 @@ public class Solution {
 	}
 	
     public void deleteNode(ListNode node) {
-        //[4,5,1,9], n = 1
-    	boolean isFindNext = true;
-    	ListNode t_temp = null;
-    	System.out.println(tempNode.val);
-    	t_temp = tempNode.next; //5
-        while(isFindNext) {
-        	
-        	if(node.val==tempNode.val) {
-        	//鏈結的第一個先忽略	
-        		tempNode = tempNode.next;
-        		isFindNext = false;
-        	}else {
-        		
-        		if(t_temp.val==node.val) { //5==1
-        			tempNode.next = tempNode.next.next;
-        			isFindNext = false;
-        		}else {
-        			System.out.println("xxxx===>"+t_temp.val);
-        			System.out.println("yyyy===>"+t_temp.next.val);
-        			t_temp = t_temp.next; //4=5
-        		}
-        		
-        	}
-    	}
+    	//解法：https://leetcode.com/problems/delete-node-in-a-linked-list/solution/
+    	node.val = node.next.val;
+        node.next = node.next.next;
     	
     }
 }
